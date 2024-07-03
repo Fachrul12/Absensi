4ef33d140c67b9cf7c5f85ffb17931274f68f158
Fachrul12 <fahrulsukma10@gmail.com>
2024-07-03 09:06:34 +0700

    scan

---
+++ database/migrations/2024_07_02_045224_create_pesertas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePesertasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pesertas', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_peserta');
+            $table->string('nama');
+            $table->string('partai');
+            $table->enum('pendukung_calon', ['paslon 1', 'paslon 2', 'paslon 3']);
+            $table->string('foto');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pesertas');
+    }
+}
